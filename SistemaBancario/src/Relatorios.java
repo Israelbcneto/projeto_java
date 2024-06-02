ac06744e7998f11cb9b6ef7398a9a71d29a8eb5a
Alyssandro1771415 <dyogo.alyssandro@gmail.com>
2024-06-02 19:48:13 -0300

    feat: Adding the backup of the database schema, remotion of the old architecture

--- SistemaBancario/src/Relatorios.java
+++
@@ -1,7 +0,0 @@
-public class Relatorios {
-    public void gerarRelatorios(Administrador admin) {
-        System.out.println("Relatório de Operações:");
-        System.out.println("Total de Saques: " + admin.getTotalSaques());
-        System.out.println("Total de Depósitos: " + admin.getTotalDepositos());
-    }
-}
